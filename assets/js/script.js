4e1b7c09db102cf335da98db630cc8ddcc621b03
WGT <williamgeorgethomas@gmail.com>
2023-01-07 22:01:00 -0600

    updated to latest version of bootstrap.  added object with api league codes.  added select menu in addition to dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,23 +15,26 @@ function init() {
 }
 
 function showLeagueTable() {
+  let leagueID = {
+    premierLeague: 39,
+    laLiga: 140,
+    serieA: 71,
+    bundesliga: 78,
+    ligue1: 61,
+  };
+
   fetch(requestUrl, options)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
 
+      let leagueLogo = `<img src="https://media.api-sports.io/football/leagues/${leagueID[1]}.png">`;
+
       let standings = data.response[0].league.standings[0];
 
       standings.forEach((standing) => {
         console.log(standing);
-        // tableEl.innerHTML += `<tr>`;
         tableEl.innerHTML += `<tr><td>${standing.rank}</td><td><img class="teamLogo" src="${standing.team.logo}"></td><td>${standing.team.name}</td><td>${standing.all.played}</td><td>${standing.goalsDiff}</td><td>${standing.points}</td><td>${standing.form}</td></tr>`;
-        // tableEl.innerHTML += `<tr><td><img src="${standing.team.logo}"></td>`;
-        // tableEl.innerHTML += `<td>${standing.team.name}</td>`;
-        // tableEl.innerHTML += `<td>${standing.points}</td>`;
-        // tableEl.innerHTML += `<td>${standing.goalsDiff}</td>`; //TODO if positive green, if negative red
-        // tableEl.innerHTML += `<td>${standing.form}</td></tr>`; //TODO change letters to symbols for W D L
-        // tableEl.innerHTML += `</tr>`;
       });
     })
     .catch((err) => console.error(err));
