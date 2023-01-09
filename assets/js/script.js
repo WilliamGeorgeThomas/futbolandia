ce45a306a8c54a1722d8d953fa6afbc10f918f2d
WGT <williamgeorgethomas@gmail.com>
2023-01-08 21:39:24 -0600

    fixed functionality with dropdown select menu. now it searches only upon user selection.  modified design.  placed button in header. began creating functions for other api calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 let tableEl = document.querySelector("#table-el");
-let requestUrl = `https://api-football-v1.p.rapidapi.com/v3/standings?season=2022&league=39`;
-let showTableButton = document.querySelector("#show-table-button");
+let logoDiv = document.querySelector("#logo-div");
+// let golDif = document.querySelector("#gol-dif");
+let tableHeader = document.querySelector("#table-header");
+let leagueButton = document.querySelector("#league-button");
+let leagueOption = document.querySelector("#league-option");
+let leagueID = {
+  premierLeague: 39,
+  laLiga: 140,
+  serieA: 71,
+  bundesliga: 78,
+  ligue1: 61,
+};
+
+// const dayjs = require("dayjs");
+// dayjs().format();
 
 const options = {
   method: "GET",
@@ -14,30 +27,103 @@ function init() {
   // home landing page state?
 }
 
+function getLeagueID(inputLeague) {
+  console.log(inputLeague);
+  if (inputLeague in leagueID) {
+    return leagueID[inputLeague];
+  } else {
+    return null;
+  }
+}
+
+function getLeagueData() {
+  //get league selection from drop down
+  //show league logo
+  // let leagueChoice = document.getElementById("league-button");
+  // let requestUrl = `https://api-football-v1.p.rapidapi.com/v3/standings?season=2022&league=${getLeagueID(leagueChoice.value)}`;
+  // console.log(requestUrl);
+  // fetch(requestUrl, options)
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //     let leagueLogo = `<img src="https://media.api-sports.io/football/leagues/${getLeagueID(leagueChoice.value)}.png">`;
+  //     logoDiv.innerHTML += leagueLogo;
+  //     let standings = data.response[0].league.standings[0];
+  //   })
+  //   .catch((err) => console.error(err));
+}
+
 function showLeagueTable() {
-  let leagueID = {
-    premierLeague: 39,
-    laLiga: 140,
-    serieA: 71,
-    bundesliga: 78,
-    ligue1: 61,
-  };
+  tableHeader.classList.remove("d-none");
+  tableEl.innerHTML = "";
+  let leagueChoice = document.getElementById("league-button");
+  // console.log(leagueChoice.options[selectedIndex.value]);
+  // console.log(leagueChoice.value);
+  let requestUrl = `https://api-football-v1.p.rapidapi.com/v3/standings?season=2022&league=${getLeagueID(leagueChoice.value)}`;
+  console.log(requestUrl);
 
   fetch(requestUrl, options)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
 
-      let leagueLogo = `<img src="https://media.api-sports.io/football/leagues/${leagueID[1]}.png">`;
+      // let leagueLogo = `<img src="https://media.api-sports.io/football/leagues/${leagueID.premierLeague}.png">`;
 
       let standings = data.response[0].league.standings[0];
 
       standings.forEach((standing) => {
         console.log(standing);
-        tableEl.innerHTML += `<tr><td>${standing.rank}</td><td><img class="teamLogo" src="${standing.team.logo}"></td><td>${standing.team.name}</td><td>${standing.all.played}</td><td>${standing.goalsDiff}</td><td>${standing.points}</td><td>${standing.form}</td></tr>`;
+        tableEl.innerHTML += `<tr><td>${standing.rank}</td><td><img class="teamLogo" src="${standing.team.logo}"></td><td>${standing.team.name}</td><td>${standing.all.played}</td><td id="golDif">${standing.goalsDiff}</td><td>${standing.points}</td><td>${standing.form}</td></tr>`;
+
+        // if (standing.goalsDiff >= 0) {
+        //   golDif.classList.add("text-success");
+        // } else {
+        //   golDif.classList.add("text-danger");
+        // }
       });
     })
     .catch((err) => console.error(err));
+}
+
+function showFixtures() {
+  //display table of fixtures for selected league
+  let leagueChoice = document.getElementById("league-button");
+  let fixtureUrl = `https://api-football-v1.p.rapidapi.com/v3/fixtures?league=39&season=2022`;
+
+  fetch(fixtureUrl, options)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      let fixtures = data.response[0].league.standings[0];
+
+      standings.forEach((standing) => {
+        console.log(standing);
+        tableEl.innerHTML += ``;
+      });
+    })
+    .catch((err) => console.error(err));
+}
+
+function showTopScorers() {
+  //display table of top scorers for selected league
+    let leagueChoice = document.getElementById("league-button");
+    let topScorerUrl = `https://api-football-v1.p.rapidapi.com/v3/players/topscorers?league=39&season=2022`;
+
+
+    fetch(scorerUrl, options)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+
+        let topScorers = data.response[0].league.standings[0];
+
+        standings.forEach((standing) => {
+          console.log(standing);
+          tableEl.innerHTML += ``;
+        });
+      })
+      .catch((err) => console.error(err));
 }
 
 //function calls
@@ -46,7 +132,7 @@ init();
 
 //event listeners
 
-showTableButton.addEventListener("click", showLeagueTable);
+leagueButton.addEventListener("change", showLeagueTable);
 
 //   fetch(requestUrl)
 //     .then(function (response) {
