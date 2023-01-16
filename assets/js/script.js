3da92cf0e4ed67cfe9e05c3dad3907f04380c285
WGT <williamgeorgethomas@gmail.com>
2023-01-15 21:08:41 -0600

    added showTopScorers function. added functionality to radio buttons. made all data clear when league changes including by adding or removing d-none class in header elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
-let tableEl = document.querySelector("#table-el");
+//global variables
+
 let logoDiv = document.querySelector("#logo-div");
-// let golDif = document.querySelector("#gol-dif");
-let tableHeader = document.querySelector("#table-header");
 let leagueButton = document.querySelector("#league-button");
 let leagueOption = document.querySelector("#league-option");
+let leagueTableHeader = document.querySelector("#league-table-header");
+let leagueTableDiv = document.querySelector("#league-table-div");
+let scorerHeader = document.querySelector("#scorer-header");
+let scorersDiv = document.querySelector("#scorers-div");
+let radioDiv = document.querySelector("#radio-div");
+let radioTable = document.querySelector("#radio-table");
+let radioFixtures = document.querySelector("#radio-fixtures");
+let radioScorers = document.querySelector("#radio-scorers");
 let leagueID = {
   premierLeague: 39,
   laLiga: 140,
@@ -11,6 +18,7 @@ let leagueID = {
   bundesliga: 78,
   ligue1: 61,
 };
+// let golDif = document.querySelector("#gol-dif");
 
 // const dayjs = require("dayjs");
 // dayjs().format();
@@ -39,24 +47,31 @@ function getLeagueID(inputLeague) {
 function getLeagueData() {
   //get league selection from drop down
   //show league logo
-  // let leagueChoice = document.getElementById("league-button");
-  // let requestUrl = `https://api-football-v1.p.rapidapi.com/v3/standings?season=2022&league=${getLeagueID(leagueChoice.value)}`;
-  // console.log(requestUrl);
-  // fetch(requestUrl, options)
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     console.log(data);
-  //     let leagueLogo = `<img src="https://media.api-sports.io/football/leagues/${getLeagueID(leagueChoice.value)}.png">`;
-  //     logoDiv.innerHTML += leagueLogo;
-  //     let standings = data.response[0].league.standings[0];
-  //   })
-  //   .catch((err) => console.error(err));
+  radioTable.checked = false;
+  radioFixtures.checked = false;
+  radioScorers.checked = false;
+  logoDiv.innerHTML = "";
+  leagueTableDiv.innerHTML = "";
+  scorersDiv.innerHTML = "";
+
+  if (!leagueTableHeader.classList.contains("d-none")) {
+    leagueTableHeader.classList.add("d-none");
+  }
+
+  if (!scorerHeader.classList.contains("d-none")) {
+    scorerHeader.classList.add("d-none");
+  }
+
+  let leagueChoice = document.getElementById("league-button");
+  let leagueLogo = `<img class="leagueLogo" src="https://media.api-sports.io/football/leagues/${getLeagueID(leagueChoice.value)}.png">`;
+  logoDiv.innerHTML += leagueLogo;
+  radioDiv.classList.remove("d-none");
 }
 
 function showLeagueTable() {
-  tableHeader.classList.remove("d-none");
-  tableEl.innerHTML = "";
-  logoDiv.innerHTML = "";
+  leagueTableHeader.classList.remove("d-none");
+  scorerHeader.classList.add("d-none");
+  scorersDiv.innerHTML = "";
   let leagueChoice = document.getElementById("league-button");
   let requestUrl = `https://api-football-v1.p.rapidapi.com/v3/standings?season=2022&league=${getLeagueID(leagueChoice.value)}`;
   console.log(requestUrl);
@@ -66,14 +81,11 @@ function showLeagueTable() {
     .then((data) => {
       console.log(data);
 
-      let leagueLogo = `<img src="https://media.api-sports.io/football/leagues/${getLeagueID(leagueChoice.value)}.png">`;
-      logoDiv.innerHTML += leagueLogo;
-
       let standings = data.response[0].league.standings[0];
 
       standings.forEach((standing) => {
         console.log(standing);
-        tableEl.innerHTML += `<tr><td>${standing.rank}</td><td><img class="teamLogo" src="${standing.team.logo}"></td><td>${standing.team.name}</td><td>${standing.all.played}</td><td id="golDif">${standing.goalsDiff}</td><td>${standing.points}</td><td>${standing.form}</td></tr>`;
+        leagueTableDiv.innerHTML += `<tr><td>${standing.rank}</td><td><img class="teamLogo" src="${standing.team.logo}"></td><td>${standing.team.name}</td><td>${standing.all.played}</td><td id="golDif">${standing.goalsDiff}</td><td>${standing.points}</td><td>${standing.form}</td></tr>`;
 
         // if (standing.goalsDiff >= 0) {
         //   golDif.classList.add("text-success");
@@ -99,7 +111,7 @@ function showFixtures() {
 
       standings.forEach((standing) => {
         console.log(standing);
-        tableEl.innerHTML += ``;
+        leagueTableDiv.innerHTML += ``;
       });
     })
     .catch((err) => console.error(err));
@@ -107,19 +119,24 @@ function showFixtures() {
 
 function showTopScorers() {
   //display table of top scorers for selected league
+  leagueTableDiv.innerHTML = "";
+  leagueTableHeader.classList.add("d-none");
+  scorerHeader.classList.remove("d-none");
   let leagueChoice = document.getElementById("league-button");
-  let topScorerUrl = `https://api-football-v1.p.rapidapi.com/v3/players/topscorers?league=39&season=2022`;
+  let topScorerUrl = `https://api-football-v1.p.rapidapi.com/v3/players/topscorers?league=${getLeagueID(leagueChoice.value)}&season=2022`;
 
-  fetch(scorerUrl, options)
+  fetch(topScorerUrl, options)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
 
-      let topScorers = data.response[0].league.standings[0];
+      let topScorers = data.response;
 
-      standings.forEach((standing) => {
-        console.log(standing);
-        tableEl.innerHTML += ``;
+      //TODO find way to display flag instead of country name next to player (leauges too?)
+
+      topScorers.forEach((scorer) => {
+        console.log(scorer);
+        scorersDiv.innerHTML += `<tr><td>${scorer.player.name} (${scorer.player.nationality})</td><td>${scorer.statistics[0].team.name}</td><td>${scorer.statistics[0].goals.total}</td></tr>`;
       });
     })
     .catch((err) => console.error(err));
@@ -131,7 +148,7 @@ init();
 
 //event listeners
 
-leagueButton.addEventListener("change", showLeagueTable);
+leagueButton.addEventListener("change", getLeagueData);
 
 //   fetch(requestUrl)
 //     .then(function (response) {
